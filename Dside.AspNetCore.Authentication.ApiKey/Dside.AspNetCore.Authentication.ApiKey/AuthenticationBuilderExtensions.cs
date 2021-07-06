0dc56470f9144a484e2b820286a65adfd942bdc4
Dmytro Iavlianskyi <dsidedp@gmail.com>
2021-07-06 20:48:43 +0200

    DelegateClaimResolver added

--- Dside.AspNetCore.Authentication.ApiKey/Dside.AspNetCore.Authentication.ApiKey/AuthenticationBuilderExtensions.cs
+++ Dside.AspNetCore.Authentication.ApiKey/Dside.AspNetCore.Authentication.ApiKey/AuthenticationBuilderExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +18,30 @@ namespace Dside.AspNetCore.Authentication.ApiKey
         {
             authenticationBuilder.Services.AddTransient<IApiKeyAuthenticationClaimsResolver, TResolver>();
             return authenticationBuilder.AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthenticationOptions.SchemeName, options);
+        }
+
+        public static AuthenticationBuilder AddApiKey(this AuthenticationBuilder authenticationBuilder, Func<string, Claim[]> resolveFunc, Action<ApiKeyAuthenticationOptions> options = null)
+        {
+            authenticationBuilder.Services.AddTransient<IApiKeyAuthenticationClaimsResolver>(sp => new DelegateClaimResolver(resolveFunc));
+            return authenticationBuilder.AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthenticationOptions.SchemeName, options);
+        }
+
+        public static AuthenticationBuilder AddApiKey(this AuthenticationBuilder authenticationBuilder, Func<IServiceProvider, string, Task<Claim[]>> resolveFunc, Action<ApiKeyAuthenticationOptions> options = null)
+        {
+            authenticationBuilder.Services.AddTransient<IApiKeyAuthenticationClaimsResolver>(sp => new DelegateClaimResolver((s) => resolveFunc(sp, s)));
+            return authenticationBuilder.AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthenticationOptions.SchemeName, options);
+        }
+
+        private class DelegateClaimResolver : IApiKeyAuthenticationClaimsResolver
+        {
+            private readonly Func<string, Task<Claim[]>> _resolveFunc;
+            
+            public int Order => 0;
+
+            public DelegateClaimResolver(Func<string, Task<Claim[]>> resolveFunc) => _resolveFunc = resolveFunc;
+            public DelegateClaimResolver(Func<string, Claim[]> resolveFunc) => _resolveFunc = (s) => Task.FromResult(resolveFunc(s));
+
+            public async Task<Claim[]> Resolve(string apiKey) => await _resolveFunc(apiKey);
         }
     }
 }
